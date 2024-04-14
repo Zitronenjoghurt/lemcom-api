5465c26ede2225c0880d81c9a0e8b10a48bfe7fd
mo8it <mo8it@proton.me>
2024-04-14 19:26:40 +0200

    Print "Listening on …"

--- src/main.rs
+++ src/main.rs
@@ -25,5 +25,7 @@ async fn main() -> io::Result<()> {
         .with_state(shared_db);
 
     let listener = tokio::net::TcpListener::bind((Ipv4Addr::new(0, 0, 0, 0), 3000)).await?;
+    println!("Listening on {}", listener.local_addr()?);
+
     axum::serve(listener, app).await
 }
